e93dbdae831665375dfa42a3c3689d2b1fa7ab6c
Mr.M.V.BALA <mvbalakrishnan95@gmail.com>
2022-08-24 13:47:00 +0530

    it is an udemy exercise.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(NumberPalindrome.isPalindrome(1221));
+        System.out.println(NumberPalindrome.isPalindrome(22));
+        System.out.println(NumberPalindrome.isPalindrome(-1221));
+        System.out.println(NumberPalindrome.isPalindrome(20982));
+
     }
 }
--- src/NumberPalindrome.java
+++ src/NumberPalindrome.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class NumberPalindrome {
+public class NumberPalindrome {
+    public static boolean isPalindrome(int number) {
+
+       number=Math.abs(number);
+      int givenNumber=number;
+        int reverseNumber = 0;
+        while (number > 0) {
+            int reverse = number % 10;
+            reverseNumber = (reverseNumber * 10) + reverse;
+            number /= 10;
+        }
+
+        if (givenNumber == reverseNumber)
+            return true;
+        else
+            return false;
+    }
 }
+
